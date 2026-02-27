ee5762a16729e30f569dededec2c1f64493805a8
skupie <114020227+skupie@users.noreply.github.com>
2026-02-28 05:32:41 +0600

    Update App.kt

--- app/src/main/java/com/radiant/sms/App.kt
+++ app/src/main/java/com/radiant/sms/App.kt
@@ -2,5 +2,5 @@ package com.radiant.sms
 
 object AppConfig {
     // IMPORTANT: Base URL must end with a slash
-    const val BASE_URL = "https://radiant.infinityfreeapp.com/"
+    const val BASE_URL = "https://basic.bd-d.online/"
 }
