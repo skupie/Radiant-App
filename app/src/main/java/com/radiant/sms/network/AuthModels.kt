09a2fdf695a8ace33d139d427c95bcc0cdfd4aeb
skupie <114020227+skupie@users.noreply.github.com>
2026-02-28 07:08:05 +0600

    Create AuthModels.kt

---
+++ app/src/main/java/com/radiant/sms/network/AuthModels.kt
@@ -0,0 +1,37 @@
+// app/src/main/java/com/radiant/sms/network/models/AuthModels.kt
+package com.radiant.sms.network.models
+
+import com.squareup.moshi.JsonClass
+
+typealias AnyJson = Map<String, Any?>
+
+@JsonClass(generateAdapter = true)
+data class LoginRequest(
+    val email: String,
+    val password: String,
+    val device_name: String? = "android"
+)
+
+@JsonClass(generateAdapter = true)
+data class LoginResponse(
+    val token: String,
+    val user: User
+)
+
+@JsonClass(generateAdapter = true)
+data class User(
+    val id: Long? = null,
+    val name: String? = null,
+    val email: String? = null,
+    val role: String? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class MeResponse(
+    val user: User
+)
+
+@JsonClass(generateAdapter = true)
+data class MessageResponse(
+    val message: String? = null
+)
