01f0f7725d72fda2e74fa147210253e39a123448
skupie <114020227+skupie@users.noreply.github.com>
2026-02-28 22:54:44 +0600

    Update ApiService.kt

--- app/src/main/java/com/radiant/sms/network/ApiService.kt
+++ app/src/main/java/com/radiant/sms/network/ApiService.kt
@@ -1,6 +1,5 @@
 package com.radiant.sms.network
 
-import com.radiant.sms.network.models.MemberShareDetailsResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
