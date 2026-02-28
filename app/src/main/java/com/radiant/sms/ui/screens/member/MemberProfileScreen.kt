244c79899781e62ff22e73bcf48df93f0b4ca8c9
skupie <114020227+skupie@users.noreply.github.com>
2026-02-28 07:27:56 +0600

    Update MemberProfileScreen.kt

--- app/src/main/java/com/radiant/sms/ui/screens/member/MemberProfileScreen.kt
+++ app/src/main/java/com/radiant/sms/ui/screens/member/MemberProfileScreen.kt
@@ -10,7 +10,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.radiant.sms.data.TokenStore
 import com.radiant.sms.network.NetworkModule
-import com.radiant.sms.network.models.MemberProfileResponse
+import com.radiant.sms.network.MemberProfileResponse
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
