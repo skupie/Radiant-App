7c85d7149cf2ded6325922e8aaaf8a2408311b5c
skupie <114020227+skupie@users.noreply.github.com>
2026-02-28 16:02:55 +0600

    Update LoginScreen.kt

--- app/src/main/java/com/radiant/sms/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/radiant/sms/ui/screens/LoginScreen.kt
@@ -9,7 +9,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.radiant.sms.storage.TokenStore
+import com.radiant.sms.data.TokenStore
 import com.radiant.sms.ui.Routes
 import com.radiant.sms.ui.viewmodel.AuthViewModel
 
