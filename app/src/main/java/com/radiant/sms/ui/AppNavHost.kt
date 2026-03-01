699f659e399d402d49d39e945bf0f4355e73119a
skupie <114020227+skupie@users.noreply.github.com>
2026-03-01 07:05:47 +0600

    Update AppNavHost.kt

--- app/src/main/java/com/radiant/sms/ui/AppNavHost.kt
+++ app/src/main/java/com/radiant/sms/ui/AppNavHost.kt
@@ -1,29 +1,62 @@
-// app/src/main/java/com/radiant/sms/ui/AppNavHost.kt
 package com.radiant.sms.ui
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.radiant.sms.ui.screens.*
-import com.radiant.sms.ui.screens.member.*
+import androidx.navigation.navArgument
+import com.radiant.sms.ui.screens.admin.AdminHomeScreen
+import com.radiant.sms.ui.screens.login.LoginScreen
+import com.radiant.sms.ui.screens.member.MemberDueSummaryScreen
+import com.radiant.sms.ui.screens.member.MemberLedgerScreen
+import com.radiant.sms.ui.screens.member.MemberProfileScreen
+import com.radiant.sms.ui.screens.member.MemberShareDetailsScreen
+import com.radiant.sms.ui.screens.splash.SplashScreen
 
 @Composable
 fun AppNavHost() {
-    val nav = rememberNavController()
+    val navController = rememberNavController()
 
-    NavHost(navController = nav, startDestination = Routes.SPLASH) {
+    NavHost(
+        navController = navController,
+        startDestination = Routes.SPLASH
+    ) {
+        composable(Routes.SPLASH) { SplashScreen(navController) }
+        composable(Routes.LOGIN) { LoginScreen(navController) }
+        composable(Routes.ADMIN_HOME) { AdminHomeScreen(navController) }
+        composable(Routes.MEMBER_HOME) { /* your existing member home screen here */ }
 
-        composable(Routes.SPLASH) { SplashScreen(nav) }
-        composable(Routes.LOGIN) { LoginScreen(nav) }
+        composable(
+            route = Routes.MEMBER_PROFILE,
+            arguments = listOf(navArgument("memberId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val memberId = backStackEntry.arguments?.getInt("memberId") ?: 0
+            MemberProfileScreen(navController, memberId)
+        }
 
-        composable(Routes.MEMBER_HOME) { MemberHomeScreen(nav) }
-        composable(Routes.ADMIN_HOME) { AdminHomeScreen(nav) }
+        composable(
+            route = Routes.MEMBER_LEDGER,
+            arguments = listOf(navArgument("memberId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val memberId = backStackEntry.arguments?.getInt("memberId") ?: 0
+            MemberLedgerScreen(navController, memberId)
+        }
 
-        // ✅ Member feature routes
-        composable(Routes.MEMBER_PROFILE) { MemberProfileScreen(nav) }
-        composable(Routes.MEMBER_LEDGER) { MemberLedgerScreen(nav) }
-        composable(Routes.MEMBER_DUE_SUMMARY) { MemberDueSummaryScreen(nav) }
-        composable(Routes.MEMBER_SHARE_DETAILS) { MemberShareDetailsScreen(nav) }
+        composable(
+            route = Routes.MEMBER_DUE_SUMMARY,
+            arguments = listOf(navArgument("memberId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val memberId = backStackEntry.arguments?.getInt("memberId") ?: 0
+            MemberDueSummaryScreen(navController, memberId)
+        }
+
+        composable(
+            route = Routes.MEMBER_SHARE_DETAILS,
+            arguments = listOf(navArgument("memberId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val memberId = backStackEntry.arguments?.getInt("memberId") ?: 0
+            MemberShareDetailsScreen(navController, memberId)
+        }
     }
 }
