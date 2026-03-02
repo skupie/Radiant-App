bc5407c10fdb1f8a3e27ea33854978145be1e3af
skupie <114020227+skupie@users.noreply.github.com>
2026-03-02 15:16:30 +0600

    Update ApiClient.kt

--- app/src/main/java/com/radiant/sms/network/ApiClient.kt
+++ app/src/main/java/com/radiant/sms/network/ApiClient.kt
@@ -9,7 +9,7 @@ object ApiClient {
 
     // ✅ Put your real base url here (example)
     // Must end with /
-    private const val BASE_URL = "https://basic.bd-d.online/"
+    private const val BASE_URL = "https://radiant.infinityfreeapp.com/"
 
     private val logger = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
