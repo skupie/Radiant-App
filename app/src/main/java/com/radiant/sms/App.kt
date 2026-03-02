422fdfde6a17031bf501d686caf2c16a75bb8904
skupie <114020227+skupie@users.noreply.github.com>
2026-03-02 15:18:28 +0600

    Update App.kt

--- app/src/main/java/com/radiant/sms/App.kt
+++ app/src/main/java/com/radiant/sms/App.kt
@@ -8,5 +8,5 @@ object AppConfig {
      * If you get HTML like "<center>openresty</center>" or "400 Bad Request",
      * you are usually hitting the wrong host.
      */
-    const val BASE_URL = "https://basic.bd-d.online/"
+    const val BASE_URL = "https://radiant.infinityfreeapp.com/"
 }
